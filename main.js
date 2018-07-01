6a05a210623bb40a3928cf8e7f084aa55f17b775
sonicakes <soniaparker1207@gmail.com>
2018-07-01 17:30:36 +1000

    added some logic to the game

---
+++ main.js
@@ -0,0 +1,51 @@
+	//first, we create a function that will generate random colours, RGB. it will take math.floor and multiply by 256
+	function randomColor() {
+		//pick a "red" from 0 - 255
+		let r = Math.floor(Math.random() * 256);
+		//pick a "green" from 0 - 255
+		let g = Math.floor(Math.random() * 256);
+		//pick a "blue" from 0 - 255
+		let b = Math.floor(Math.random() * 256);
+		return `rgb(${r}, ${g}, ${b})`;
+	}
+	//at this point we know how to generate one RGB colour. 
+	function generateRandomColors(num) {
+		//make an array
+		let arr = [];
+		//add num random colors to arr
+		for(let i = 0; i < num; i++) {
+			//get random color and push into arr
+			arr.push(randomColor());
+		}
+		//return that array
+        return arr;
+    }
+        //we need 3 random colours so 
+        let numSquares = 3;
+const colors = generateRandomColors(numSquares);
+console.log(colors);
+
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    console.log( "ready!" );
+//make the pink boxes display the colors generate rgb colors from the "colors" array
+$("#1").css("background-color", colors[0]);
+$("#2").css("background-color", colors[1]);
+$("#3").css("background-color", colors[2]);
+
+//pick the computer choice by random and 
+let color = Math.floor(Math.random() * colors.length);
+let colorToGuess = colors[color];
+console.log(`computer has chosen ${colorToGuess}`);
+
+//then display the rgb of one of colors array as "colorToGuess" in the #message board
+$("#message").text(colorToGuess);
+
+//when we click on the square and it matches the color on the message board, we need to say on the message board: "you've won!" and change the body background to the winning color
+$(".box").on("click", function(){
+    
+})
+
+
+
+});
